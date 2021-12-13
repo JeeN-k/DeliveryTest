ab910c986938d6c3cf0b26ecb6dee3ff688bdc80
Oleg Stepanov <olezhka106s@gmail.com>
2021-12-13 19:11:53 +0300

    Initial Commit

---
+++ DeliveryTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryTest
+//
+//  Created by Oleg Stepanov on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
